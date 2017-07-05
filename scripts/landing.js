8a3f2535693ac8a187b2eb0f4c593e87207061e2
aaron-stewart <aaron.stewart.contact@gmail.com>
2017-07-05 12:55:23 -0500

    fix animatePoints function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,28 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    pointsArray[index].style.opacity = 1;
+    pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+    pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+    pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 }
 
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
+var animatePoints = function() {
+    forEach(pointsArray, revealPoint)
 };
 
 window.onload = function() {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var sellingPoints = document.getElementsByClassName('selling-points')[0]
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
     window.addEventListener("scroll", function(event) {
         //Automatically animate points on a tall screen that can't trigger event
         if (window.innerHeight > 950) {
-          animatePoints(pointsArray);
+          animatePoints()
         }
 
-        If (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints()
         }
     });
 }
