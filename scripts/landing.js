6473d8b8b7021ad476238df3b7920edd5c90bfc6
aaron-stewart <aaron.stewart.contact@gmail.com>
2017-07-12 17:29:52 -0500

    16-jquery landing page complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(index) {
-    pointsArray[index].style.opacity = 1;
-    pointsArray[index].style.transform = "scaleX(1) translateY(0)";
-    pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
-    pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-}
-
 var animatePoints = function() {
-    forEach(pointsArray, revealPoint)
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0]
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
+    }
 
-    window.addEventListener("scroll", function(event) {
-        //Automatically animate points on a tall screen that can't trigger event
-        if (window.innerHeight > 950) {
-          animatePoints()
-        }
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints()
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+          animatePoints();
         }
     });
-}
+});
